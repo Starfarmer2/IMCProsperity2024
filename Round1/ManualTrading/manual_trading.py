d9dcde85dfeb59f4ef96ba184624bcb865905c62
Jasper Hu <wh169@duke.edu>
2024-04-12 00:44:51 -0400

    add manual trading

---
+++ Round1/ManualTrading/manual_trading.py
@@ -0,0 +1,14 @@
+
+# def find_profit(L: int,H: int, k: float=1) -> float:
+#     p1 = k/100*(1000-L)*(L-900)
+#     p2 = k/100*(H-L)*(1000-H)
+#     return p1+p2
+
+# #Case 933, 966
+# print(find_profit(920,967))
+
+def lin(x):
+    return x**2/2-900*x
+
+print(lin(900))
+
--- Round1/manual_trading.py
+++
@@ -1,9 +0,0 @@
-
-def find_profit(L: int,H: int, k: float=1) -> float:
-    p1 = k/100*(1000-L)*(L-900)
-    p2 = k/100*(H-L)*(1000-H)
-    return p1+p2
-
-#Case 933, 966
-print(find_profit(920,967))
-
